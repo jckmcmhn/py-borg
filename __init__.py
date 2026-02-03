5996988670e67715c75e3c6c7f01b3a41b1b8097
jack <j.mcmahon@outlook.ie>
2026-02-03 22:39:26 +0000

    initial commit, functions for basic combat and two sample yaml configs

---
+++ __init__.py
@@ -0,0 +1,130 @@
+from random import randint, choice
+import re
+import yaml
+
+MANUAL_DICE_ROLLS = False
+
+print("Starting the game")
+print(f"MANUAL_DICE_ROLLS is {MANUAL_DICE_ROLLS}\n\n")
+
+def roll_dice(nd):
+    if MANUAL_DICE_ROLLS:
+        result = int(input(f"Roll {nd} and type in the result"))
+    else:
+        nd = nd.lower()
+        if bool(re.search("^[0-9]*d[0-9]*$", nd)):
+            split_nd = nd.split("d")
+            n = int(split_nd[0])
+            d = int(split_nd[1])
+        else:
+            raise ValueError("Invalid roll description")
+        result = 0
+        for i in range(0,n):
+            roll = randint(1,d)
+            result += roll
+    return result
+
+class Weapon:
+    def __init__(self, config):
+        self.name = config["name"]
+        self.type = config["type"]
+        self.damage = config["damage"]
+        self.dr = config.get("dr",12)
+        self.category = config["category"]
+        self.primary = config.get("primary",False)
+
+    def __str__(self):
+        return "It's a %s called %s it deals %s" % (self.category, self.name, self.damage)
+
+class Character:
+    def greet(self, greeting):
+        print(greeting)
+
+    def set_weapons(self):
+        weapons = []
+        primary_weapon = None
+        for item in self.items:            
+            if "weapon" in item:
+                weapon = Weapon(item["weapon"])
+                weapons.append(weapon)
+                if weapon.primary:
+                    primary_weapon = weapon
+        if primary_weapon is None:
+            print("No primary weapon set, choosing at random")
+            primary_weapon = choice(weapons)
+        print(f"{self.name}'s primary weapon is {primary_weapon}")
+        self.primary_weapon = primary_weapon
+        self.weapons = weapons
+
+    def __init__(self, config):
+        self.name = config["name"]
+        self.is_pc = config["is_pc"]
+        self.max_hp = config["hp"]
+        self.current_hp = config["hp"]
+        self.abilities = config["abilities"]
+        self.items = config["items"]
+        if self.is_pc is False:
+            self.morale = config["morale"]
+        self.description = config.get("description")
+        print(self.description)
+        if "greeting" in config:
+            self.greet(config["greeting"])
+        self.alive = True
+        self.set_weapons()
+
+    def am_i_dead():
+        # Well?
+        if self.alive is False:
+            print(f"{self.name}:I'm dead folks!")
+    
+   
+    def roll_broken(self):
+        print("Death roll") # Not crazy about this whole "broken" concept
+        broken_roll = roll_dice("1d4")
+        if broken_roll == 4:
+            print("DEAD")
+            self.alive = False
+
+    def take_standard_damage(self,damage):
+        print(f"{self.name} took {damage} damage")
+        self.current_hp -= self.current_hp
+        if self.current_hp == 0:
+            print("Bad times for you friend")
+            self.roll_broken()
+        elif self.current_hp < 0:
+            print("DEAD")
+            self.alive = False
+        else:
+            print(f"{self.name} is on {self.current_hp}")
+            self.alive = True
+
+    def make_standard_attack(self, weapon, target):
+        print(f"{self.name} attacks {target.name} with {weapon.name}")
+        dr = weapon.dr
+        attack_role = randint(1,20)
+        if weapon.type == "melee":
+            attack_role += self.abilities["strength"]
+        if weapon.type == "ranged":
+            attack_role += self.abilities["presence"]
+        if attack_role >= dr:
+            print(f"{self.name} hits!")
+            damage = roll_dice(weapon.damage)
+            target.take_standard_damage(damage)
+        else:
+            print(f"{self.name} misses")
+
+with open("pc_sample.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.SafeLoader)
+
+urvarg = Character(config)
+
+with open("pc_sample_2.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.SafeLoader)
+rolf = Character(config)
+
+
+while (urvarg.alive) and (rolf.alive):
+    urvarg.make_standard_attack(urvarg.primary_weapon, rolf)
+    if rolf.alive:
+        rolf.make_standard_attack(rolf.primary_weapon, urvarg)
+
