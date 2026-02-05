f93b91a29b1a22d97847828417acfbd721c1cac4
jack <j.mcmahon@outlook.ie>
2026-02-05 20:00:55 +0000

    add armour

--- __init__.py
+++ __init__.py
@@ -3,11 +3,14 @@ import re
 import yaml
 
 MANUAL_DICE_ROLLS = False
+ALLOW_ATTACK_ALLIES = True
+
 
 print("Starting the game")
 print(f"MANUAL_DICE_ROLLS is {MANUAL_DICE_ROLLS}\n\n")
 
 def roll_dice(nd):
+    # TODO: allow people not to set n
     if MANUAL_DICE_ROLLS:
         result = int(input(f"Roll {nd} and type in the result"))
     else:
@@ -35,6 +38,14 @@ class Weapon:
 
     def __str__(self):
         return "It's a %s called %s it deals %s" % (self.category, self.name, self.damage)
+    
+class Armour:
+    def __init__(self, config):
+        self.type = config["type"]
+        self.dice = config["dice"]
+
+    def __str__(self):
+        return "It's a %s armour, it provides %s damage reduction" % (self.type, self.dice)
 
 class Character:
     def greet(self, greeting):
@@ -56,8 +67,11 @@ class Character:
         self.primary_weapon = primary_weapon
         self.weapons = weapons
 
-    def __init__(self, config):
-        self.name = config["name"]
+    def __init__(self, config, name = None):
+        if name is not None:
+            self.name = name
+        else:
+            self.name = config["name"]
         self.is_pc = config["is_pc"]
         self.max_hp = config["hp"]
         self.current_hp = config["hp"]
@@ -71,6 +85,11 @@ class Character:
             self.greet(config["greeting"])
         self.alive = True
         self.set_weapons()
+        if config["armour"] is not None:
+            self.armour = Armour(config["armour"])
+        else:
+            self.armour = None
+        
 
     def am_i_dead():
         # Well?
@@ -86,6 +105,10 @@ class Character:
             self.alive = False
 
     def take_standard_damage(self,damage):
+        if self.armour is not None:
+            armour_reduction = roll_dice(self.armour.dice)
+            print(f"reducing damage by {armour_reduction} due to armour")
+            damage -= armour_reduction
         print(f"{self.name} took {damage} damage")
         self.current_hp -= self.current_hp
         if self.current_hp == 0:
@@ -101,7 +124,8 @@ class Character:
     def make_standard_attack(self, weapon, target):
         print(f"{self.name} attacks {target.name} with {weapon.name}")
         dr = weapon.dr
-        attack_role = randint(1,20)
+        attack_role = roll_dice("1d20")
+        # critical and fumble rolls go here
         if weapon.type == "melee":
             attack_role += self.abilities["strength"]
         if weapon.type == "ranged":
@@ -112,6 +136,16 @@ class Character:
             target.take_standard_damage(damage)
         else:
             print(f"{self.name} misses")
+
+    def get_available_actions(self, others):
+        action_tuples = []
+        for other in others:
+            if len(self.weapons):
+                action_tuples += [ (other, weapon) for weapon in self.weapons ]
+        print(action_tuples)
+        return action_tuples
+
+
 
 with open("pc_sample.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.SafeLoader)
@@ -128,3 +162,5 @@ while (urvarg.alive) and (rolf.alive):
     if rolf.alive:
         rolf.make_standard_attack(rolf.primary_weapon, urvarg)
 
+actions = rolf.get_available_actions([urvarg])
+rolf.make_standard_attack(actions[0][1], actions[0][0])
