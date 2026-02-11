e7d8bbd19089a5084a5645715f3715875c13d648
jack <j.mcmahon@outlook.ie>
2026-02-11 20:20:56 +0000

    moved character class into separate file

---
+++ classes/character.py
@@ -0,0 +1,376 @@
+from random import choice
+import logging
+from classes import roll_dice
+from classes.armour import Armour
+from classes.equipment import Scroll, General, Weapon
+
+class Character:
+    def __init__(self, config, settings, name = None):
+        if name is not None:
+            self.name = name
+        else:
+            self.name = config["name"]
+        self.settings = settings
+        self.description = config.get("description")
+        print(self.description)
+        if "greeting" in config:
+            self.greet(config["greeting"])
+        self.max_hp = config["hp"]
+        self.current_hp = config["hp"]
+        self.alive = True
+        self.is_pc = config["is_pc"]
+        self.actions_this_turn = 0 #TODO: RAW are a bit vague on if there is a difference between attacks and actions (which could be using an item), but for now I'm treating them as the same
+        if self.is_pc:
+            self.abilities = config["abilities"]
+            self.defence = config["abilities"]["agility"] # This can be modified separate to standard agility tests
+            self.items = config["items"]
+            self.init_weapons = config["weapons"]
+            self.make_standard_attack = self.pc_attack_with_weapon
+            self.set_scrolls(self.items) #TODO: It would be nice to be able to add new scrolls
+            self.set_equipment(self.items)
+            print("Getting powers for today")
+            self.powers = roll_dice("1d4", self.settings["manual_dice"] in ["pc_only", "always"]) + self.abilities["presence"]
+        else:
+            self.morale = config["morale"]
+            self.size = config.get("size", 2)
+            self.make_standard_attack = self.npc_attack_with_weapon
+        pronouns = config.get("pronouns", "they/them/their")
+        pronouns_split = pronouns.split("/")
+        if len(pronouns_split) != 3:
+            pronouns_split = ["they","them","their"]
+        self.subject = pronouns_split[0]
+        self.third = pronouns_split[1] # TODO: read up on grammar terms
+        self.possessive = pronouns_split[2]
+        
+        self.weapons = config.get("weapons",[])
+        self.set_weapons(True)
+        if config["armour"] is not None:
+            self.armour = Armour(config["armour"], self, self.settings)
+        else:
+            self.armour = None
+        if self.settings["manual_dice"] in ["always", "pc_only"]:
+            self.decision_function = self.manual_action
+        else:
+            self.decision_function = self.random_action
+        self.dizzy = False # TODO: If true: During this time, Powers will always fail in the worst possible way.
+
+    def greet(self, greeting):
+        print('"' + greeting + '"')
+
+    def set_weapons(self, is_init = False):
+        print(f"Setting weapons for {self.name}")
+        self.primary_weapon = None
+        self.secondary_weapon = None
+        if is_init:
+            #We have to convert all the config into Weapon objects
+            weapons = []
+            for weapon in self.weapons:
+                weapon = Weapon(self.settings, weapon)
+                weapons.append(weapon)
+                if weapon.rank == "primary":
+                    self.primary_weapon = weapon
+                elif weapon.rank == "secondary":
+                    self.secondary_weapon = weapon
+        else:
+            weapons = self.weapons
+        if len(weapons) == 0:
+            self.primary_weapon = Weapon(self.settings) #unarmed
+            self.secondary_weapon = Weapon(self.settings)
+        elif len(weapons) == 1:
+            self.primary_weapon = weapons[0]
+            self.secondary_weapon = Weapon(self.settings)
+        else:
+            if len(weapons) > 2:
+                print(f"{self.name} is carrying {len(weapons)} weapons in {self.possessive} inventory. {len(weapons) - 2} will have to go in {self.possessive} bag")
+            if self.primary_weapon is None:
+                print("No primary weapon set, choosing at random")
+                self.primary_weapon = choice(weapons)
+        if self.secondary_weapon is None:
+            print("No secondary weapon set, choosing at random")
+            self.secondary_weapon = choice([weapon for weapon in weapons if weapon != self.primary_weapon.name])
+        print(f"{self.name}'s primary weapon is {self.primary_weapon.name}")
+        print(f"{self.name}'s secondary weapon is {self.secondary_weapon.name}")
+        self.weapons = weapons
+
+    def destroy_weapon(self, weapon):
+        logging.debug(f"{self.name}'s weapon {weapon.name} is being destroyed")
+        if weapon.category == "unarmed":
+            logging.debug("Cannot destroy an unarmed weapon")
+        else:
+            for weapon_object in self.weapons:
+                if weapon_object.id == weapon.id:
+                    self.weapons.remove(weapon_object)
+                    print(f"{self.name}'s weapon {weapon.name} has been destroyed")
+                    self.set_weapons(False)
+                    break
+
+    def set_scrolls(self, items):
+        scrolls = []
+        scroll_codes = []
+        for item in items:
+            if item["type"] == "scroll":
+                scroll = Scroll(item["name"], item["flavour_name"], self.settings)
+                scrolls.append(scroll)
+                scroll_codes.append(scroll.scroll_code)
+        print("Here are the scrolls " + ", ".join([scroll.name for scroll in scrolls]))
+        self.scrolls = scrolls
+        self.scroll_codes = scroll_codes
+
+    def set_equipment(self, items):
+        print(items)
+        equipment = []
+        for item in items:
+            if item["type"] != "scroll":
+                i = General(item["name"], item["dice"], self.settings)
+                equipment.append(i)
+        print("Here are the scrolls " + ", ".join([i.name for i in equipment]))
+        self.equipment = equipment
+
+    def random_action(self, actions):
+        return choice(actions)
+    
+    def manual_action(self, actions):
+        print("\n\nHere are the available options\n")
+        for i, action in enumerate(actions):
+            print(f"Option {i}: ")
+            if isinstance(action[1], Weapon):
+                print(f"Use {action[1].name} on {action[0].name}")
+            elif isinstance(action[1], Scroll):
+                print(f"Use {action[1].name} on {','.join([a.name for a in action[0]])}")
+        decision = int(input("\nWhich option? Just type the number: "))
+        return actions[decision]
+
+    def get_obs(self, audience):
+        if self.alive is False:
+            return {}
+        if self.is_pc: 
+            obs = {
+                "p_weapon_dice": self.primary_weapon.dice, # an observant human GM would know this #TODO: Though maybe not on round 1
+                "s_weapon_dice": self.secondary_weapon.dice, # an observant human GM would know this #TODO: Though maybe not on round 1
+                "scroll_codes": self.scroll_codes, # TODO: I would like to eventually make this "scrolls that audience has seen" but for now...
+                "armour_dice": self.armour.dice, # an observant human GM would know this #TODO: Though maybe not on round 1
+                "dizzy": self.dizzy,
+                "extra_actions_this_turn": self.actions_this_turn
+            }
+            if audience == "friends": # for now, let's say all PCs have thorough knowledge of their team mates states
+                obs["max_hp"] = self.max_hp
+                obs["current_hp"] = self.current_hp
+                obs["strength"] = self.abilities["strength"]
+                obs["presence"] = self.abilities["presence"]
+                obs["agility"] = self.abilities["agility"]
+                obs["toughness"] = self.abilities["toughness"]
+                obs["defence"] = self.defence
+                obs["powers"] = self.powers
+                obs["number_items"] = self.items #TODO: This should ideally be # of useful items, or # of items by category
+
+        elif not self.is_pc:
+            obs = {
+                "p_weapon_dice": self.primary_weapon.dice, # an observant human player would know this
+                "s_weapon_dice": self.secondary_weapon.dice, # an observant human player would know this
+                "size": self.size,
+                "morale": self.morale, #TODO: PCs shouldn't know this pre-morale roll
+            }
+        return obs
+
+    def am_i_dead(self):
+        # Well?
+        if self.alive is False or self.current_hp < 0:
+            msg = choice([
+                f"{self.name}: I'm dead folks!",
+                f"I guess that's the last we'll see of ol' {self.name}",
+                f"{self.name}'s dead Jim",
+                f"That's a wrap on {self.name}",
+            ])
+            print(msg)
+            return True
+        else:
+            return False
+   
+    def roll_broken(self):
+        logging.debug("Death roll") # Not crazy about this whole "broken" concept
+        broken_roll = roll_dice("1d4", self.settings["manual_dice"] in ["always", "npc_only"])
+        if broken_roll == 4:
+            print(f"{self.name} is DEAD")
+            self.alive = False
+
+    def apply_damage(self,damage):
+        if (self.armour is not None) and (not self.armour.dice.startswith("0d2")):
+            logging.debug(f"Incoming damage is {damage}. Rolling for armour")
+            armour_reduction = roll_dice(self.armour.dice, self.settings["manual_dice"] in ["always"])
+            logging.debug(f"{self.name} has {self.current_hp} HP before taking damage.")
+            print(f"Reducing damage by {armour_reduction} due to armour")
+            damage -= armour_reduction
+        else:
+            print(f"{self.name} has no armour")
+        if damage > 0:
+            self.current_hp -= damage
+            print(f"{self.name} took {damage} damage and is {self.current_hp} HP now")
+            if self.current_hp == 0:
+                self.roll_broken()
+            elif self.current_hp < 0:
+                print(f"{self.name} is DEAD")
+                self.alive = False
+            else:
+                print(f"{self.name} is on {self.current_hp}")
+                self.alive = True
+            return self.alive
+        else:
+            print("No damage done")
+
+    def apply_healing(self,heal):
+        if heal > 0:
+            self.current_hp += heal
+            self.current_hp = min(self.current_hp, self.max_hp)
+            print(f"{self.name} healed {heal} damage and is {self.current_hp} HP now")
+            return self.alive
+        else:
+            print("No healing done")
+
+    def make_defence_roll(self, attacker):
+        defence_roll = roll_dice("1d20", self.settings["manual_dice"] in ["always", "pc_only"])
+        fumble = False
+        if defence_roll == 20:
+            print("CRITICAL DEFENCE WIN")
+            self.actions_this_turn += 1
+        elif defence_roll == 1:
+            print("DEFENCE FUMBLE")
+            fumble = True
+        defence_roll += self.defence + self.settings["to_dodge"]
+        dr = 12
+        logging.debug(f"DR is {dr}, roll result is {defence_roll}")
+        if defence_roll >= dr:
+            logging.debug(f"{self.name} dodged the attack from {attacker.name}")
+            return True, fumble
+        else:
+            logging.debug(f"{self.name} did not dodge the attack from {attacker.name}")
+            return False, fumble
+
+    def use_equipment(self, action):
+        equipment = action[1]
+        target = action[0]
+        logging.debug(f"{self.name} is using {equipment.name} on {action[0].name}")
+        equipment.use(target)
+        equipment.count -= 1
+        print(f"Ther are {equipment.count} {equipment.name}s left")
+
+
+    def pc_attack_with_weapon(self, target, weapon):
+        print(f"{self.name} (PC) attacks {target.name} with {weapon.name}")
+        dr = weapon.dr
+        multiplier = 1
+        critical = False
+        attack_roll = roll_dice("1d20", self.settings["manual_dice"] in ["always", "pc_only"])
+        if attack_roll == 20:
+            critical = True
+            print("CRITICAL")
+            multiplier = 2
+        if attack_roll == 1:
+            self.destroy_weapon(weapon)
+        # fumble rolls go here
+        else:
+            attack_roll += self.settings["to_hit"]
+            if weapon.type == "melee":
+                attack_roll += self.abilities["strength"]
+            elif weapon.type == "ranged":
+                attack_roll += self.abilities["presence"]
+            logging.debug(f"DR is {dr}, roll result is {attack_roll}, critical is {critical}")
+            if critical or attack_roll >= dr: # Presumably Crits always hit?
+                print(f"{self.name} hits! Rolling for damage.")
+                damage = roll_dice(weapon.dice, self.settings["manual_dice"] in ["always", "pc_only"])
+                logging.debug(f"Damage roll is {damage}, multiplier is {multiplier}")
+                target_alive = target.apply_damage(multiplier * damage)
+                if target_alive and critical and target.armour is not None:
+                    target.armour.reduce_tier(target,1)
+            else:
+                print(f"{self.name} misses")
+            if self.settings["manual_dice"] in ["pauses"]:
+                input("--Continue--")
+
+
+    def npc_attack_with_weapon(self, target, weapon):
+        print(f"{self.name} (NPC) attacks {target.name} with {weapon.name}")
+        multiplier = 1
+        missed, fumble = target.make_defence_roll(self)
+        if fumble:
+            multiplier = 2
+        if missed:
+            print(f"{self.name} misses.")
+        else:
+            print(f"{self.name} hits! Rolling for damage.")
+            damage = roll_dice(weapon.dice, self.settings["manual_dice"] in ["always","npc_only"])
+            logging.debug(f"Damage roll is {damage}, multiplier is {multiplier}")
+            target_alive = target.apply_damage(multiplier * damage)
+            if fumble and target_alive is True:
+                print(f"{target.name}'s armour is damaged")
+                target.armour.reduce_tier(target, 1)
+
+    def npc_attack_with_weapon_on_npc(self, target, weapon):
+        # To handle this edge-case, assume any npc on npc attack hit automatically
+        # TODO: Give NPCs placeholder defence stats to handle this better
+        print(f"{self.name} (NPC) attacks {target.name} with {weapon.name}")
+        multiplier = 1
+        damage = multiplier * roll_dice(weapon.dice, self.settings["manual_dice"] in ["always", "npc_only"])
+        target.apply_damage(damage)
+
+    def get_available_actions(self, allies, enemies):
+        #TODO: Need to filter based on ALLOW_ATTACK_ALLIES
+        weapon_actions = []
+        scroll_actions = []
+        equipment_actions = []
+        others = allies + enemies
+        if self.settings["allow_attack_allies"]:
+            targets = others
+        else:
+            targets = enemies
+        for other in targets:
+            weapon_actions += [ (other, weapon) for weapon in [self.primary_weapon, self.secondary_weapon] ]
+        if self.is_pc is True:
+            if self.powers > 1 and (self.scrolls is not None):
+                for scroll in self.scrolls:
+                    scroll_actions = scroll.list_actions(allies, enemies, self)
+            if self.equipment is not None:
+                for item in self.equipment:
+                    if item.count > 0: #Pretty embarassed not to remember this sooner, Urm was on -13 medicine chests
+                        equipment_actions = item.list_actions(allies, enemies, self)
+        return scroll_actions + weapon_actions + equipment_actions
+    
+    def start_turn(self, allies, enemies):
+        print("------------------------------")
+        print(f"{self.name} is starting {self.possessive} turn")
+        if self.am_i_dead():
+            logging.warning(f"{self.name} is supposed to be dead. Something has gone wrong here")
+            print("------------------------------")
+            return None
+        self.actions_this_turn += 1
+        for _ in range(0, self.actions_this_turn):
+            actions = self.get_available_actions(allies, enemies)
+            action = self.decision_function(actions)
+            # Scrolls can attack more than one Character, so this bit needs to handle them differently
+            if isinstance(action[1], Scroll):
+                action[1].use(self, action[0])
+            elif isinstance(action[1], General):
+                print("Using a non-scroll action")
+                self.use_equipment(action)
+            else:
+                if action[0].is_pc and self.is_pc: # this might be the usecase for is_ally?
+                    logging.warning(f"{self.name} is attacking their ally {action[0].name}")
+                    self.make_standard_attack(action[0], action[1])
+                elif (not action[0].is_pc) and (not self.is_pc):
+                    logging.warning(f"{self.name} is attacking their ally {action[0].name}")
+                    self.npc_attack_with_weapon_on_npc(action[0], action[1])
+                else:
+                    self.make_standard_attack(action[0], action[1])
+                self.last_target = action[0]
+            print("------------------------------")
+            return action[0]
+        # TODO: Check for status effects
+        # TODO: Check if dead after status effects
+        # Get actions
+        # Decide on action
+        # Take action
+        # Log results of action
+        # Check if dead before ending turn
+
+    def __str__(self):
+        return f"A character called {self.name}. {self.description}"
+    
